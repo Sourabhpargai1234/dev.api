8ca7fd8cb4da9bdb8ce795baae15b4f4ef60ca94
Sourabh <Sourabhpar@gmail.com>
2024-06-29 22:59:59 +0530

    first commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: 'https://dev-api-frontend.vercel.app/',
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
   credentials: true, // Enable credentials (cookies, authorization headers)
